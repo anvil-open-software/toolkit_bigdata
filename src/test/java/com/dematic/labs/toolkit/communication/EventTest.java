30554646ed3ef1952230648807f318f060f431ce
mitchemr <michael.mitchell@dematic.com>
2015-05-21 10:50:15 -0700

    DLABS-266, cleanup generating events.

--- src/test/java/com/dematic/labs/toolkit/communication/EventTest.java
+++ src/test/java/com/dematic/labs/toolkit/communication/EventTest.java
@@ -4,15 +4,17 @@ import org.joda.time.DateTime;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
 import java.util.UUID;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.junit.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
 
 public final class EventTest {
     @Test
     public void convertEventToJson() throws IOException {
-        // 1) test event to json then to event
+        // test event to json then to event
         final Event rawEvent = new Event(UUID.randomUUID(), 1, 5, DateTime.now(), 1234.34);
         final String jsonEvent = EventUtils.eventToJson(rawEvent);
         final Event fromJson = EventUtils.jsonToEvent(jsonEvent);
@@ -21,5 +23,13 @@ public final class EventTest {
         assertThat(rawEvent.getNodeId(), is(fromJson.getNodeId()));
         assertThat(rawEvent.getTimestamp(), is(fromJson.getTimestamp()));
         assertThat(rawEvent.getValue(), is(fromJson.getValue()));
+    }
+
+    @Test
+    public void generateEvents() {
+        final long numberOfEvents = 1000000;
+        // test generation of events
+        final List<Event> events = EventTestingUtils.generateEvents(numberOfEvents, 20, 50);
+        assertEquals(numberOfEvents, events.size());
     }
 }
