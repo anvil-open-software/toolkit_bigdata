83f7022c1abd1c8dad07054fc3d7553a96aa8e29
mitchemr <michael.mitchell@dematic.com>
2015-07-06 14:10:32 -0700

    DLABS-316, synchronise

--- src/main/java/com/dematic/labs/toolkit/Circular.java
+++ src/main/java/com/dematic/labs/toolkit/Circular.java
@@ -3,14 +3,15 @@ package com.dematic.labs.toolkit;
 import com.google.common.collect.HashMultiset;
 import com.google.common.collect.Iterables;
 import com.google.common.collect.Multiset;
+import com.google.common.util.concurrent.Monitor;
 
 import java.util.Iterator;
 import java.util.List;
 
 public final class Circular<T> {
     private final Multiset<T> counter;
-
     private final Iterator<T> elements;
+    private final Monitor monitor = new Monitor();
 
     public Circular(final List<T> elements) {
         counter = HashMultiset.create();
@@ -18,9 +19,14 @@ public final class Circular<T> {
     }
 
     public T getOne() {
-        final T element = this.elements.next();
-        counter.add(element);
-        return element;
+        monitor.enter();
+        try {
+            final T element = this.elements.next();
+            counter.add(element);
+            return element;
+        } finally {
+            monitor.leave();
+        }
     }
 
     public int getCount(final T element) {
