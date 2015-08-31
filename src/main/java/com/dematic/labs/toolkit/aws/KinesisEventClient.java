e13cb73870f87e8ac8b4c38ef68cb0c186f98392
mitchemr <michael.mitchell@dematic.com>
2015-08-31 14:35:23 -0700

    DLABS-362, handling failures.

--- src/main/java/com/dematic/labs/toolkit/aws/KinesisEventClient.java
+++ src/main/java/com/dematic/labs/toolkit/aws/KinesisEventClient.java
@@ -2,13 +2,9 @@ package com.dematic.labs.toolkit.aws;
 
 import com.amazonaws.services.kinesis.AmazonKinesisAsyncClient;
 import com.amazonaws.services.kinesis.AmazonKinesisClient;
-import com.amazonaws.services.kinesis.model.PutRecordRequest;
-import com.amazonaws.services.kinesis.model.PutRecordResult;
-import com.amazonaws.services.kinesis.model.PutRecordsRequest;
-import com.amazonaws.services.kinesis.model.PutRecordsRequestEntry;
-import com.amazonaws.services.kinesis.model.PutRecordsResult;
-import com.amazonaws.services.kinesis.model.PutRecordsResultEntry;
+import com.amazonaws.services.kinesis.model.*;
 import com.dematic.labs.toolkit.communication.Event;
+import com.google.common.collect.ImmutableSet;
 import com.jayway.awaitility.Awaitility;
 import com.jayway.awaitility.core.ConditionTimeoutException;
 import org.slf4j.Logger;
@@ -18,12 +14,15 @@ import java.io.IOException;
 import java.math.BigInteger;
 import java.nio.ByteBuffer;
 import java.util.List;
+import java.util.Optional;
 import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Future;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 import java.util.stream.LongStream;
 
 import static com.dematic.labs.toolkit.aws.Connections.getAmazonAsyncKinesisClient;
@@ -39,6 +38,9 @@ import static com.dematic.labs.toolkit.communication.EventUtils.generateEvents;
  */
 public class KinesisEventClient {
     private static final Logger LOGGER = LoggerFactory.getLogger(KinesisEventClient.class);
+
+    private static final Set<String> RETRYABLE_ERR_CODES = ImmutableSet.of("ProvisionedThroughputExceededException",
+            "InternalFailure", "ServiceUnavailable");
 
     private KinesisEventClient() {
     }
@@ -76,37 +78,71 @@ public class KinesisEventClient {
                 FixedBatchSpliterator.withBatchSize(LongStream.range(1, numberOfEvents + 1).boxed(), streamChunkingSize).
                         parallel().
                         forEach(event -> {
-                            final PutRecordsRequest putRecordsRequest = new PutRecordsRequest();
-                            putRecordsRequest.setStreamName(kinesisInputStream);
+                            // 1) generate batched events and dispatch request
                             final List<PutRecordsRequestEntry> putRecordsRequestEntries =
                                     generatePutRecordsRequestEntries(kinesisRecordsPerRequest, nodeSize, orderSize);
-                            putRecordsRequest.setRecords(putRecordsRequestEntries);
-
-                            final Future<PutRecordsResult> futurePutRecordsResult =
-                                    amazonKinesisClient.putRecordsAsync(putRecordsRequest);
-                            PutRecordsResult putRecordsResult = null;
-                            try {
-                                putRecordsResult = futurePutRecordsResult.get();
-                            } catch (final InterruptedException | ExecutionException resultEx) {
-                                LOGGER.error("unable to retrieve kinesis results: forkJoinPool {}",
-                                        forkJoinPool.toString(), resultEx);
-                            }
-                            if (putRecordsResult != null && putRecordsResult.getFailedRecordCount() > 0) {
-                                LOGGER.error("failed pushed events: {}", putRecordsResult.getFailedRecordCount());
-                                final List<PutRecordsResultEntry> records = putRecordsResult.getRecords();
-                                records.stream().parallel().forEach(record -> {
-                                    if (record.getErrorCode() != null) {
-                                        LOGGER.error("reason: {} : {} --> {}", record.getErrorCode(),
-                                                record.getErrorMessage(), record.getShardId());
-                                    }
-                                });
-                            }
+                            dispatch(amazonKinesisClient, kinesisInputStream, putRecordsRequestEntries, 5);
                         });
             }).get();
         } catch (final InterruptedException | ExecutionException ex) {
             LOGGER.error("unexpected exception:", ex);
         } finally {
             forkJoinPool.shutdownNow();
+        }
+    }
+
+    private static void dispatch(final AmazonKinesisAsyncClient amazonKinesisClient, final String stream,
+                                 final List<PutRecordsRequestEntry> putRecordsRequestEntries, final int numberOfTries) {
+        int count = 0;
+        final PutRecordsRequest putRecordsRequest = new PutRecordsRequest();
+        putRecordsRequest.setStreamName(stream);
+        putRecordsRequest.setRecords(putRecordsRequestEntries);
+        PutRecordsResult putRecordsResult = null;
+        
+        do {
+
+            try {
+                final Future<PutRecordsResult> futurePutRecordsResult =
+                        amazonKinesisClient.putRecordsAsync(putRecordsRequest);
+                putRecordsResult = futurePutRecordsResult.get();
+                // deal with any type of system, connection exception, etc...
+            } catch (final Throwable any) {
+                LOGGER.error("unexpected exception dispatching to kinesis, trying again : count = {}", count, any);
+            } finally {
+                count++;
+            }
+
+            // deal with kinesis exception, provision...
+            if (putRecordsResult == null) {
+                continue;
+            }
+
+            if (putRecordsResult.getFailedRecordCount() > 0) {
+                LOGGER.error("failed pushed events: {}, trying again : count = {}",
+                        putRecordsResult.getFailedRecordCount(), count);
+                final List<PutRecordsResultEntry> failed = putRecordsResult.getRecords();
+                // retrieve the failed events
+                final List<PutRecordsRequestEntry> retries = IntStream.range(0, failed.size())
+                        .mapToObj(i -> {
+                            final PutRecordsRequestEntry retry = putRecordsRequestEntries.get(i);
+                            final String errorCode = failed.get(i).getErrorCode();
+                            // only retry accepeted error codes
+                            if (errorCode != null && RETRYABLE_ERR_CODES.contains(errorCode)) {
+                                return Optional.of(retry);
+                            } else {
+                                return Optional.<PutRecordsRequestEntry>empty();
+                            }
+                        })
+                        .filter(Optional::isPresent)
+                        .map(Optional::get)
+                        .collect(Collectors.toList());
+                // set the failed request to be tried again
+                putRecordsRequest.setRecords(retries);
+            }
+        } while (putRecordsResult != null && putRecordsResult.getFailedRecordCount() > 0 && count < numberOfTries);
+
+        if (putRecordsResult != null && putRecordsResult.getFailedRecordCount() > 0) {
+            LOGGER.error("unable to dispatch: {} events", putRecordsResult.getFailedRecordCount());
         }
     }
 
