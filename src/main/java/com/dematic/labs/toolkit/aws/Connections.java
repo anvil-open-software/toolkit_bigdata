50a2678ea6c27401db9b1ad0eb6d2e07038ae0de
mitchemr <michael.mitchell@dematic.com>
2015-10-14 13:53:41 -0700

    DLABS-457 -- Cached driver.

--- src/main/java/com/dematic/labs/toolkit/aws/Connections.java
+++ src/main/java/com/dematic/labs/toolkit/aws/Connections.java
@@ -22,7 +22,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-import redis.clients.jedis.JedisPoolConfig;
 
 import java.util.concurrent.Executors;
 
