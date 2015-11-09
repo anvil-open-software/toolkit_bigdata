b52813cef01065811b9c8234d366ced72d13f093
mitchemr <michael.mitchell@dematic.com>
2015-11-09 14:21:02 -0800

    DLABS-488, initial checkin for node simulator.

--- src/main/java/com/dematic/labs/toolkit/simulators/NodeExecutor.java
+++ src/main/java/com/dematic/labs/toolkit/simulators/NodeExecutor.java
@@ -1,11 +1,15 @@
 package com.dematic.labs.toolkit.simulators;
 
 import com.dematic.labs.toolkit.CountdownTimer;
-import com.dematic.labs.toolkit.communication.EventUtils;
+import com.dematic.labs.toolkit.communication.Event;
+import com.dematic.labs.toolkit.communication.EventSequenceNumber;
 import com.google.common.util.concurrent.RateLimiter;
+import org.joda.time.DateTime;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.Random;
+import java.util.UUID;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.TimeUnit;
@@ -13,20 +17,28 @@ import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import static com.dematic.labs.toolkit.aws.kinesis.KinesisEventClient.dispatchEventsToKinesis;
+import static com.dematic.labs.toolkit.communication.EventUtils.*;
+import static java.util.Collections.singletonList;
 
 public final class NodeExecutor {
     private static final Logger LOGGER = LoggerFactory.getLogger(NodeExecutor.class);
 
-    final int nodeRangeSize;
-    final Stream<String> nodeRangeIds;
-    final int maxEventsPerMinutePerNode;
+    private final int nodeRangeSize;
+    private final Stream<String> nodeRangeIds;
+    private final int maxEventsPerMinutePerNode;
+    private final int avgInterArrivalTime;
+    private final String generatorId;
 
-    public NodeExecutor(final int nodeRangeMin, final int nodeRangeMax, final int maxEventsPerMinutePerNode) {
+    public NodeExecutor(final int nodeRangeMin, final int nodeRangeMax, final int maxEventsPerMinutePerNode,
+                        final int avgInterArrivalTime, final String generatorId) {
         nodeRangeSize = nodeRangeMax - nodeRangeMin;
         nodeRangeIds = IntStream.range(nodeRangeMin, nodeRangeMax).mapToObj(i -> "Node-" + i);
         this.maxEventsPerMinutePerNode = maxEventsPerMinutePerNode;
-        LOGGER.info("NodeExecutor: created with a nodeRangeSize {} between {} and {} with maxEventsPerMinutePerNode {}",
-                nodeRangeSize, nodeRangeMin, nodeRangeMax, maxEventsPerMinutePerNode);
+        this.avgInterArrivalTime = avgInterArrivalTime;
+        this.generatorId = generatorId;
+        LOGGER.info("NodeExecutor: created with a nodeRangeSize {} between {} and {} with maxEventsPerMinutePerNode {} "
+                        + "and an averger inter-arrival time of {} and generatorId {}", nodeRangeSize, nodeRangeMin,
+                nodeRangeMax, maxEventsPerMinutePerNode, avgInterArrivalTime, generatorId);
     }
 
     public void execute(final Long durationInMinutes, final String kinesisEndpoint, final String kinesisStreamName) {
@@ -37,11 +49,10 @@ public final class NodeExecutor {
             nodeRangeIds.forEach(nodeId -> forkJoinPool.submit(() -> {
                 dispatchPerNode(kinesisEndpoint, kinesisStreamName, nodeId, durationInMinutes, latch);
             }));
-
             // wait 5 minutes longer then duration
             latch.await(durationInMinutes + 5, TimeUnit.MINUTES);
         } catch (final Throwable any) {
-            any.printStackTrace();
+            LOGGER.error("NodeExecutor: Unhandled Error: stopping execution", any);
         } finally {
             System.out.println("finally");
         }
@@ -55,9 +66,10 @@ public final class NodeExecutor {
             final CountdownTimer countdownTimer = new CountdownTimer();
             countdownTimer.countDown((int) TimeUnit.MINUTES.toMinutes(durationInMinutes));
             final RateLimiter rateLimiter = RateLimiter.create(eventsPerSecond(maxEventsPerMinutePerNode));
+            final Random randomNumberGenerator = new Random();
             // dispatch until duration ends at a rate specified by max events
             while (true) {
-                dispatch(kinesisEndpoint, kinesisStreamName, nodeId, rateLimiter);
+                dispatch(kinesisEndpoint, kinesisStreamName, nodeId, rateLimiter, randomNumberGenerator);
                 if (countdownTimer.isFinished()) {
                     LOGGER.info("NodeExecutor: Completed dispatching events for node {}", nodeId);
                     break;
@@ -69,37 +81,34 @@ public final class NodeExecutor {
     }
 
     private void dispatch(final String kinesisEndpoint, final String kinesisStreamName, final String nodeId,
-                          final RateLimiter rateLimiter) {
+                          final RateLimiter rateLimiter, final Random randomNumberGenerator) {
         rateLimiter.acquire();
-       //todo dispatchEventsToKinesis(kinesisEndpoint, kinesisStreamName, EventUtils.generateEvents());
+        // event time is based on the avgInterArrivalTime * bounded random int, 6 is the upper bounds, inclusive
+        final DateTime now = now().plusSeconds(randomNumberGenerator.nextInt(7) * avgInterArrivalTime);
+        // todo: come back to batching and errors
+        dispatchEventsToKinesis(kinesisEndpoint, kinesisStreamName, singletonList(new Event(UUID.randomUUID(),
+                EventSequenceNumber.next(), nodeId, null, now, generatorId, null)));
     }
 
     private static double eventsPerSecond(final int eventsPerMinutes) {
         return (double) Math.round((eventsPerMinutes / 60d) * 100) / 100;
     }
 
-    /**
-     * Generates notifications with Node Id’s that are formed by the String: “Node-” followed by a
-     * number between the minimum and maximum given in 1.a. converted to a String and padded with
-     * zeros to the left so that nodes can be lexicographically ordered up to 100,000 nodes.
-     * These are referred to as “Generator Nodes”
-     * <p>
-     * <p>
-     * <p>
-     * <p>
-     * Generates Notifications at a rate equal or lower than the maximum given in 1.b for each node Id of its “Generator Nodes”
-     * <p>
-     * The timestamps of the generated Notifications are such that the inter-arrival time (see definition above) is distributed according to either a uniform distribution with bounds: 0 and 6 times MIAT or an exponential distribution with mean = MIAT
-     * <p>
-     * The notifications do not need to be generated in an order that is congruent with the order of its timestamps.
-     * <p>
-     * <p>
-     * For this first implementation the rates will be of the order of tens or hundreds of notifications per second.
-     */
-
-
     public static void main(String[] args) {
-        final NodeExecutor nodeExecutor = new NodeExecutor(100, 200, 5);
-        nodeExecutor.execute(5L, "", "");
+        if (args == null || args.length != 8) {
+            throw new IllegalArgumentException();
+        }
+        final int nodeRangeMin = Integer.valueOf(args[0]);
+        final int nodeRangeMax = Integer.valueOf(args[1]);
+        final int maxEventsPerMinutePerNode = Integer.valueOf(args[2]);
+        final int avgInterArrivalTime = Integer.valueOf(args[3]);
+        final long duriationInMinutes = Long.valueOf(args[4]);
+        final String kinesisEndpoint = args[5];
+        final String kinesisStreamName = args[6];
+        final String generatorId = args[7];
+
+        final NodeExecutor nodeExecutor = new NodeExecutor(nodeRangeMin, nodeRangeMax, maxEventsPerMinutePerNode,
+                avgInterArrivalTime, generatorId);
+        nodeExecutor.execute(duriationInMinutes, kinesisEndpoint, kinesisStreamName);
     }
 }
