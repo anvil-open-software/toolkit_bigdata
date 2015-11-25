093504e97f265c014db3ba41b36b8eccee3a5ae5
mitchemr <michael.mitchell@dematic.com>
2015-11-25 09:35:09 -0800

    DLABS-498, added a generic builder.

---
+++ src/main/java/com/dematic/labs/toolkit/GenericBuilder.java
@@ -0,0 +1,34 @@
+package com.dematic.labs.toolkit;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+import java.util.function.Supplier;
+
+public final class GenericBuilder<T> {
+    private final Supplier<T> instantiator;
+
+    private List<Consumer<T>> instanceModifiers = new ArrayList<>();
+
+    public GenericBuilder(Supplier<T> instantiator) {
+        this.instantiator = instantiator;
+    }
+
+    public static <T> GenericBuilder<T> of(Supplier<T> instantiator) {
+        return new GenericBuilder<>(instantiator);
+    }
+
+    public <U> GenericBuilder<T> with(BiConsumer<T, U> consumer, U value) {
+        final Consumer<T> c = instance -> consumer.accept(instance, value);
+        instanceModifiers.add(c);
+        return this;
+    }
+
+    public T build() {
+        final T value = instantiator.get();
+        instanceModifiers.forEach(modifier -> modifier.accept(value));
+        instanceModifiers.clear();
+        return value;
+    }
+}
