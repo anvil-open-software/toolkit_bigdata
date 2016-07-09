6ed6544f23022f1995f573f2f637ba72ce45c9d8
dematic-denis-antonioli <denis.antonioli@dematic.com>
2016-07-08 17:00:53 -0700

    DLABS-766: ensure system properties are reset after test completion

--- src/test/java/com/dematic/labs/toolkit/SystemPropertyRule.java
+++ src/test/java/com/dematic/labs/toolkit/SystemPropertyRule.java
@@ -4,35 +4,60 @@ import org.junit.rules.ExternalResource;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
 
+import static java.lang.String.format;
+import static java.lang.System.clearProperty;
+import static java.lang.System.getProperty;
+import static java.lang.System.setProperty;
+import static java.nio.file.Files.newInputStream;
+import static java.nio.file.Paths.get;
+
+
+/**
+ * load system properties from the junit.properties file
+ */
 public final class SystemPropertyRule extends ExternalResource {
     private static final Logger LOGGER = LoggerFactory.getLogger(SystemPropertyRule.class);
-    // load system properties from the junit.properties file
-    @Override
-    protected void before() throws Throwable {
-        final String junitPropertiesPath = String.format("%s/.m2/junit.properties", System.getProperty("user.home"));
-        final Stream<String> propertiesStream = Files.lines(Paths.get(junitPropertiesPath));
+    private final Map<String, String> previousProperties = new HashMap<>();
 
-        final List<String> properties = propertiesStream.collect(Collectors.toList());
-        for (final String property : properties) {
-            final String[] split = property.split("=");
-            final String propertyKey = split[0];
-            if (!propertyKey.isEmpty() && !propertyKey.startsWith("#") && !propertyKey.startsWith("nexus")) {
-                final String propertyValue = split[1];
-                // special case for kinesisInputStream
-                if (propertyKey.equals("kinesisInputStream")) {
-                    final String kinesisInputStream = String.format("%s_stream", System.getProperty("user.name"));
-                    System.setProperty(propertyKey, kinesisInputStream);
+    @Override
+    protected void before() throws IOException {
+        final Path junitPropertiesPath = get(getProperty("user.home"), ".m2", "junit.properties");
+        try (final InputStream inputStream = newInputStream(junitPropertiesPath)) {
+            final Properties junitProperties = new Properties();
+            junitProperties.load(inputStream);
+            for (final String propertyKey : junitProperties.stringPropertyNames()) {
+                if (propertyKey.startsWith("nexus")) {
+                    continue;
+                }
+                if ("kinesisInputStream".equals(propertyKey)) {
+                    final String kinesisInputStream = format("%s_stream", getProperty("user.name"));
+                    put(propertyKey, kinesisInputStream);
                     LOGGER.info("created kinesis stream >{}<", kinesisInputStream);
                 } else {
-                    // add to system properties
-                    System.setProperty(propertyKey, propertyValue);
+                    put(propertyKey, junitProperties.getProperty(propertyKey));
                 }
+            }
+        }
+    }
+
+    public void put(final String propertyKey, final String propertyValue) {
+        previousProperties.put(propertyKey, setProperty(propertyKey, propertyValue));
+    }
+
+    @Override
+    protected void after() {
+        for (final Map.Entry<String, String> property : previousProperties.entrySet()) {
+            if (property.getValue() == null) {
+                clearProperty(property.getKey());
+            } else {
+                setProperty(property.getKey(), property.getValue());
             }
         }
     }
