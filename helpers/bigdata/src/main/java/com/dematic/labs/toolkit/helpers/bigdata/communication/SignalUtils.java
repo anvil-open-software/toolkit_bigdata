5ad81356d2690a1c06945fa00ff1076a299e039b
mitchemr <michael.mitchell@dematic.com>
2017-01-07 09:36:01 -0800

    DLABS-988, Support for structured streaming persistent driver.

--- helpers/bigdata/src/main/java/com/dematic/labs/toolkit/helpers/bigdata/communication/SignalUtils.java
+++ helpers/bigdata/src/main/java/com/dematic/labs/toolkit/helpers/bigdata/communication/SignalUtils.java
@@ -24,6 +24,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
+@SuppressWarnings("WeakerAccess")
 public final class SignalUtils {
     private static final Logger LOGGER = LoggerFactory.getLogger(SignalUtils.class);
     private final static ObjectMapper objectMapper;
