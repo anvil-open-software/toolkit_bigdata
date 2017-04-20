f13cafe64dd7a284ac6d3da13cffc7a4b131bd4f
mitchemr <michael.mitchell@dematic.com>
2017-04-20 13:21:38 -0700

    DLABS-1168, removed statistics.

--- helpers/simulators/src/main/java/com/dematic/labs/toolkit/helpers/simulators/grainger/OpcTagReadingExecutor.java
+++ helpers/simulators/src/main/java/com/dematic/labs/toolkit/helpers/simulators/grainger/OpcTagReadingExecutor.java
@@ -1,12 +1,6 @@
 package com.dematic.labs.toolkit.helpers.simulators.grainger;
 
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.ResultSet;
-import com.datastax.driver.core.Row;
-import com.datastax.driver.core.Session;
 import com.dematic.labs.toolkit.helpers.bigdata.CountdownTimer;
-import com.dematic.labs.toolkit.helpers.bigdata.communication.SignalValidation;
-import com.dematic.labs.toolkit.helpers.simulators.Statistics;
 import com.google.common.util.concurrent.RateLimiter;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -16,7 +10,6 @@ import org.slf4j.LoggerFactory;
 
 import java.nio.charset.Charset;
 import java.time.Instant;
-import java.util.List;
 import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ForkJoinPool;
@@ -29,21 +22,16 @@ import static com.dematic.labs.toolkit.helpers.bigdata.kafka.Connections.getKafk
 
 public final class OpcTagReadingExecutor {
     private static final Logger LOGGER = LoggerFactory.getLogger(OpcTagReadingExecutor.class);
-    private static final boolean VALIDATE = System.getProperty("dematiclabs.driver.validate.counts") != null;
 
     // EX: 100 110 30 3 10.102.20.10:9092,10.102.20.11:9092 mm_test 10.102.20.30 mm_test sparkUserId sparkPassword test_app
     private static final String HELP = "OpcTagReadingExecutor " +
-            "opcTagRangeMin opcTagRangeMax maxSignalsPerMinutePerOpcTag durationInMinutes kafkaServerBootstrap kafkaTopics\n" +
-            "... and, if dematiclabs.driver.validate.counts is set:\n" +
-            "Cassandra_Server Cassandra_Keyspace  Cassandra_Username Cassandra_Password\n" + "" +
-            "... always followed by\n" +
+            "opcTagRangeMin opcTagRangeMax maxSignalsPerMinutePerOpcTag durationInMinutes kafkaServerBootstrap kafkaTopics " +
             "ApplicationName generatorId";
 
     private final int opcTagRangeSize;
     private final Stream<String> opcTagRangeIds;
     private final int maxSignalsPerMinutePerOpcTag;
     private final String generatorId;
-    private static final Statistics STATISTICS = new Statistics();
 
     private OpcTagReadingExecutor(final int opcTagRangeMin, final int opcTagRangeMax,
                                   final int maxSignalsPerMinutePerOpcTag, final String generatorId) {
@@ -69,11 +57,6 @@ public final class OpcTagReadingExecutor {
             LOGGER.error("OpcTagReadingExecutor: Unhandled Error: stopping execution", any);
         } finally {
             try {
-                LOGGER.info("OpcTagReadingExecutor: Total Success: {}", STATISTICS.getTotalSuccessCounts());
-                LOGGER.info("OpcTagReadingExecutor: Total Errors: {}", STATISTICS.getTotalErrorCounts());
-            } catch (final Throwable ignore) {
-            }
-            try {
                 forkJoinPool.shutdownNow();
             } catch (final Throwable ignore) {
             }
@@ -97,9 +80,6 @@ public final class OpcTagReadingExecutor {
                 dispatchSingleSignal(kafkaProducer, kafkaTopics, opcTagId, rateLimiter, randomNumberGenerator);
 
                 if (countdownTimer.isFinished()) {
-                    LOGGER.debug("OpcTagReadingExecutor: Completed dispatching signals for {} ", opcTagId);
-                    LOGGER.debug("\tOpcTagReadingExecutor: {} : Success {}", opcTagId, STATISTICS.getTotalSuccessCountsById(opcTagId));
-                    LOGGER.debug("\tOpcTagReadingExecutor: {} : Error {}", opcTagId, STATISTICS.getTotalErrorCountsById(opcTagId));
                     break;
                 }
             }
@@ -135,14 +115,7 @@ public final class OpcTagReadingExecutor {
             final RecordMetadata recordMetadata = send.get();
             LOGGER.debug("OpcTagReadingExecutor: {} successfully sent >{}< to {}", opcTagId, signal,
                     recordMetadata.topic());
-            // increment counts
-            if (send.isDone() && !send.isCancelled()) {
-                STATISTICS.incrementSuccessCountById(opcTagId);
-            } else {
-                STATISTICS.incrementErrorCountById(opcTagId);
-            }
         } catch (final Throwable any) {
-            STATISTICS.incrementErrorCountById(opcTagId);
             LOGGER.error(String.format("OpcTagReadingExecutor: Unexpected error: dispatching signal to >%s<",
                     kafkaTopics), any);
         }
@@ -175,16 +148,6 @@ public final class OpcTagReadingExecutor {
         final String kafkaServerBootstrap = args[4];
         final String kafkaTopics = args[5];
 
-        // 1) check validation, table exist
-        if (VALIDATE) {
-            try {
-                validateAndCreateTable(args[6], args[7], args[8], args[9]);
-            } catch (final Throwable any) {
-                LOGGER.error("OpcTagReadingExecutor: can't validate table >{}.signal_validation<", args[7], any);
-                Runtime.getRuntime().halt(0);
-            }
-        }
-
         final String generatorId;
         if (args.length == 12) {
             generatorId = args[11];
@@ -197,48 +160,7 @@ public final class OpcTagReadingExecutor {
                     opcTagRangeMax, maxSignalsPerMinutePerOpcTag, generatorId);
             opcTagReadingExecutor.execute(durationInMinutes, kafkaServerBootstrap, kafkaTopics);
         } finally {
-            if (VALIDATE) {
-                LOGGER.info("OpcTagReadingExecutor: publishing statistics to server >{}<", args[6]);
-                try {
-                    publishStatistics(args[6], args[7], args[8], args[9], args[10]);
-                    LOGGER.info("OpcTagReadingExecutor: completed publishing statistics to server >{}<", args[6]);
-                } catch (final Throwable any) {
-                    LOGGER.error("OpcTagReadingExecutor: unable to publish statistics to server >{}<", args[6], any);
-                    Runtime.getRuntime().halt(0);
-                }
-            }
             Runtime.getRuntime().halt(0);
-        }
-    }
-
-    private static void validateAndCreateTable(final String serverAddress, final String keyspace, final String username,
-                                               final String password) {
-
-        final Cluster cluster = Cluster.builder().withCredentials(username, password).
-                addContactPoints(serverAddress).build();
-        try (final Session session = cluster.connect(keyspace)) {
-            final ResultSet execute = session.execute(SignalValidation.createCounterTableCql(keyspace));
-            final List<Row> all = execute.all();
-            all.forEach(row -> LOGGER.info(row.toString()));
-        }
-    }
-
-    private static void publishStatistics(final String serverAddress, final String keyspace, final String username,
-                                          final String password, final String appName) {
-        final Cluster cluster = Cluster.builder().withCredentials(username, password).
-                addContactPoints(serverAddress).build();
-        try (final Session session = cluster.connect(keyspace)) {
-            final ResultSet execute =
-                    session.execute(
-                            String.format("Update %s.signal_validation SET producer_count = producer_count + %s, " +
-                                            "producer_error_count = producer_error_count + %s WHERE id='%s'",
-                                    keyspace,
-                                    STATISTICS.getTotalSuccessCounts(),
-                                    STATISTICS.getTotalErrorCounts(),
-                                    appName
-                            ));
-            final List<Row> all = execute.all();
-            all.forEach(row -> LOGGER.info(row.toString()));
         }
     }
 }
