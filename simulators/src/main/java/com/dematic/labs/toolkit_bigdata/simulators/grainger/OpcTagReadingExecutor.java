f266a61163b3c239614c8ab81f9e446d9ea45269
mitchemr <michael.mitchell@dematic.com>
2017-06-22 10:49:46 -0700

    DLABS-1243, fixed logging.

--- simulators/src/main/java/com/dematic/labs/toolkit_bigdata/simulators/grainger/OpcTagReadingExecutor.java
+++ simulators/src/main/java/com/dematic/labs/toolkit_bigdata/simulators/grainger/OpcTagReadingExecutor.java
@@ -129,11 +129,13 @@ public final class OpcTagReadingExecutor {
         return Math.round(val);
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         try {
             // configuration comes from the application.conf for the driver
             final OpcTagReaderConfiguration config = new OpcTagReaderConfiguration.Builder().build();
             new OpcTagReadingExecutor(config).execute();
+        } catch (final Throwable any) {
+            LOGGER.error("Producer failed to startup:", any);
         } finally {
             Runtime.getRuntime().halt(0);
         }
