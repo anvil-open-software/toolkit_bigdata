627782b5d7b248fd483ed85b2fe14b15566d5d0c
mitchemr <michael.mitchell@dematic.com>
2017-09-22 10:58:09 -0700

    DLABS-1452, removed DSP simulators and dependencies.

--- simulators/src/main/scala/com/dematic/labs/toolkit_bigdata/simulators/Aggregation.scala
+++
@@ -1,8 +0,0 @@
-package com.dematic.labs.toolkit_bigdata.simulators
-
-import com.dematic.labs.toolkit_bigdata.simulators.configuration.MinimalProducerConfiguration
-
-object Aggregation extends App {
-  // load all the configuration
-  private val config = new MinimalProducerConfiguration.Builder().build
-}
